1528a6a14f1f9b13b018e3c8b1ff4c5240359744
juanjo210106 <jgamlop2101@g.educaand.es>
2025-11-04 19:02:22 +0100

    Realización práctica 2

---
+++ PRÁCTICA 2/WpfApp5/WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxEntrada_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            string textoSeleccionado = TextBoxEntrada.SelectedText;
+
+            if (string.IsNullOrEmpty(textoSeleccionado))
+            {
+                TextBoxMonitor.Text = "No has seleccionado nada...";
+            } else
+            {
+                TextBoxMonitor.Text = "Has seleccionado: " + textoSeleccionado;
+            }
+        }
+    }
+}
---
+++ PRÁCTICA 2/WpfApp8/WpfApp8/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChkTodos_Checked(object sender, RoutedEventArgs e)
+        {
+            bool estadoMarcado = (ChkTodos.IsChecked == true);
+
+            ChkDI.IsChecked = estadoMarcado;
+            ChkSGE.IsChecked = estadoMarcado;
+            ChkAD.IsChecked = estadoMarcado;
+        }
+
+        private void ChkModulo_Click(object sender, RoutedEventArgs e)
+        {
+            int contador = 0;
+
+            if (ChkDI.IsChecked == true) contador++;
+            if (ChkSGE.IsChecked == true) contador++;
+            if (ChkAD.IsChecked == true) contador++;
+
+            if (contador == 3)
+            {
+                ChkTodos.IsChecked = true;
+            } else if (contador == 0)
+            {
+                ChkTodos.IsChecked = false;
+            } else
+            {
+                ChkTodos.IsChecked = null;
+            }
+        }
+    }
+}
